74f38c83a018b7aff9c4e35dc7ecca9fc2110689
dimous <kasimowsky@gmail.com>
2026-02-20 15:45:40 +1000

    поднял табы

--- src/presentation/navigation/TabNavigator.tsx
+++ src/presentation/navigation/TabNavigator.tsx
@@ -3,68 +3,74 @@ import HomeScreen from "../screens/HomeScreen";
 import HomeIcon from "../../assets/icons/HomeIcon";
 import ParkingIcon from "../../assets/icons/ParkingIcon";
 import ParkingHistoryScreen from "../screens/ParkingHistoryScreen";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Tab = createBottomTabNavigator();
 
-export default () => (
-    <Tab.Navigator
-        screenOptions={
-            {
-                headerShown: false,
-                sceneStyle: {
-                    backgroundColor: "#ffffff",
-                },
-                tabBarStyle: {
-                    height: 92,
-                    shadowOffset: {
-                        width: 0,
-                        height: 0,
-                    },
-                    elevation: 20,
-                    paddingTop: 10,
-                    shadowRadius: 20,
-                    borderTopWidth: 1,
-                    shadowOpacity: 0.2,
-                    shadowColor: "#000000",
-                    backgroundColor: "#ffffff",
-                },
-            }
-        }
-    >
-        <Tab.Screen
-            name={
-                ERoutes.HOME
-            }
-            component={
-                HomeScreen
-            }
-            options={
+export default () => {
+    const insets = useSafeAreaInsets();
+
+    return (
+        <Tab.Navigator
+            screenOptions={
                 {
-                    tabBarLabelStyle: {
-                        marginTop: 3,
+                    headerShown: false,
+                    sceneStyle: {
+                        backgroundColor: "#ffffff",
                     },
-                    tabBarLabel: "Главная",
-                    tabBarIcon: ({ size, color }) => <HomeIcon size={size} color={color} />,
+                    tabBarStyle: {
+                        height: 92,
+                        shadowOffset: {
+                            width: 0,
+                            height: 0,
+                        },
+                        elevation: 20,
+                        paddingTop: 10,
+                        shadowRadius: 20,
+                        borderTopWidth: 1,
+                        shadowOpacity: 0.2,
+                        shadowColor: "#000000",
+                        marginBottom: insets.bottom,
+                        backgroundColor: "#ffffff",
+                    },
                 }
             }
-        />
-        <Tab.Screen
-            name={
-                ERoutes.PARKING_HISTORY
-            }
-            component={
-                ParkingHistoryScreen
-            }
-            options={
-                {
-                    tabBarLabelStyle: {
-                        marginTop: 3,
-                    },
-                    tabBarLabel: "История парковок",
-                    tabBarIcon: ({ size, color }) => <ParkingIcon size={size} color={color} />,
+        >
+            <Tab.Screen
+                name={
+                    ERoutes.HOME
                 }
-            }
-        />
-    </Tab.Navigator>
-);
+                component={
+                    HomeScreen
+                }
+                options={
+                    {
+                        tabBarLabelStyle: {
+                            marginTop: 3,
+                        },
+                        tabBarLabel: "Главная",
+                        tabBarIcon: ({ size, color }) => <HomeIcon size={size} color={color} />,
+                    }
+                }
+            />
+            <Tab.Screen
+                name={
+                    ERoutes.PARKING_HISTORY
+                }
+                component={
+                    ParkingHistoryScreen
+                }
+                options={
+                    {
+                        tabBarLabelStyle: {
+                            marginTop: 3,
+                        },
+                        tabBarLabel: "История парковок",
+                        tabBarIcon: ({ size, color }) => <ParkingIcon size={size} color={color} />,
+                    }
+                }
+            />
+        </Tab.Navigator>
+    );
+}
