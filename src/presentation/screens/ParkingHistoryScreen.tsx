ab33767981cb2d7f8239be9bf4e5bf9ac2b32bab
Dmitry <kasimowsky@gmail.com>
2026-03-14 20:18:29 +1000

    Remove paddingBottom from ParkingHistoryScreen styles
    Removed paddingBottom from style object.

--- src/presentation/screens/ParkingHistoryScreen.tsx
+++ src/presentation/screens/ParkingHistoryScreen.tsx
@@ -13,7 +13,6 @@ export default () => {
                         paddingTop: insets.top,
                         paddingLeft: insets.left,
                         paddingRight: insets.right,
-                        paddingBottom: insets.bottom,
                     }
                 ]
             }
